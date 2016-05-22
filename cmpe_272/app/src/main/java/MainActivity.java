3f595da478d0c25fef98250439f45fb73a6f0229
anu3990 <anushree0390@gmail.com>
2016-05-21 18:31:29 -0700

    Android Project

---
+++ cmpe_272/app/src/main/java/MainActivity.java
@@ -0,0 +1,5 @@
+/**
+ * Created by anush on 5/20/2016.
+ */
+public class MainActivity {
+}
---
+++ cmpe_272/app/src/main/java/com/example/drivequickstart/ShowWebChartActivity.java
@@ -0,0 +1,66 @@
+package com.example.drivequickstart;
+
+/**
+ * Created by anush on 5/21/2016.
+ */
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+
+@SuppressLint("SetJavaScriptEnabled")
+public class ShowWebChartActivity extends ActionBarActivity {
+
+    WebView webView;
+    int num1, num2, num3, num4, num5;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.abc_screen_simple);
+
+        Intent intent = getIntent();
+        num1 = intent.getIntExtra("NUM1", 0);
+        num2 = intent.getIntExtra("NUM2", 0);
+        num3 = intent.getIntExtra("NUM3", 0);
+        num4 = intent.getIntExtra("NUM4", 0);
+        num5 = intent.getIntExtra("NUM5", 0);
+
+        webView = (WebView)findViewById(R.id.icon);
+        webView.addJavascriptInterface(new WebAppInterface(), "Android");
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.loadUrl("file:///android_asset/chart.html");
+    }
+
+    public class WebAppInterface {
+
+        @JavascriptInterface
+        public int getNum1() {
+            return num1;
+        }
+
+        @JavascriptInterface
+        public int getNum2() {
+            return num2;
+        }
+
+        @JavascriptInterface
+        public int getNum3() {
+            return num3;
+        }
+
+        @JavascriptInterface
+        public int getNum4() {
+            return num4;
+        }
+
+        @JavascriptInterface
+        public int getNum5() {
+            return num5;
+        }
+    }
+
+}
